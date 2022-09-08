9106410a0314aa4f3c7a4834dee31018ca8f63da
aatick <113114466+aatick33@users.noreply.github.com>
2022-09-08 09:38:27 -0400

    Update streamlit_app.py
    Added more items to menu display

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthly Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamli.text('Hard-Boiled Free-Range Egg')
+
